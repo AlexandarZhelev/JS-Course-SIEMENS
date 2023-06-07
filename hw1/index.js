ed21fb993c2b1e3e4654b1c99296e73dc099939e
AlexandarZhelev <aleksandar.zhelev@siemens.com>
2023-06-07 19:00:42 +0300

    homework1

---
+++ hw1/index.js
@@ -0,0 +1,28 @@
+var firstName = prompt("Enter your first name");
+var lastName = prompt("Enter your last name");
+var age = prompt("Enter your age");
+var gender = prompt("enter your gender");
+
+console.log(`Welcome to Grandma's Basement ${firstName} ${lastName}`);
+
+console.log(`Your age is: ${age}`);
+console.log(`Gender: ${gender}`);
+
+var wine = "apple";
+var meat = "smoked";
+var marmalade = "plum";
+var papers = "picled";
+var pig = "purse";
+
+var appleWineQuantity = 10;
+var smokedMeatQuantity = 5;
+var plumMarmaladeQuantity = 9;
+var picledPapersQuantity = 4;
+var pigPurseValue = 184.35;
+
+//не разбрах много добре какво се искаше и не съм сигурен дали това трябваше да направя :(
+var appleWineId = "C7544_10";
+var smokedMeatId = "M7441_5";
+var plumMarmaladeId = "S6491_9";
+var picledPapersId = "P7485_4";
+var pursePigId = "B6584_184.35";
